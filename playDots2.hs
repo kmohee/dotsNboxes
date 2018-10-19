ad1dd1f1fe0721014eb5cf31b3a67d0fc33685b0
kmohee <37249251+kmohee@users.noreply.github.com>
2018-10-18 21:13:25 -0700

    Update playDots2.hs

--- playDots2.hs
+++ playDots2.hs
@@ -7,36 +7,13 @@ import System.IO
 import Text.Read
 import Data.Maybe
 
-
--- type Score = (Int, Int) -- myboxes, thyboxes
+-- (Result, Turn) can be thought as a world state
 
 data Turn = Computer     -- Computer
           | Person       -- Human player
         deriving (Eq, Show)
 
--- playDots :: Game -> Result -> Player -> Turn -> IO ()
-
-{-
-playDots game start opponent turn =
-    let ContinueGame(State (played ,(myboxes, thyboxes)) avail size) = start in
-    do
-        putStrLn "Please input your desired board width: "
-        n <- getLine
-        let num = read n
-        let newStart = ContinueGame (restart num)
-        putStrLn ("The Score is: "++" mine: " ++ show myboxes++ "its: " ++ show thyboxes)
-        putStrLn "Who starts? Choose 0=you, 1=computer, 2=exit."
-        line <- getLine
-        if line == "0"
-           then
-               me_play game newStart opponent Person
-           else if line == "1"
-           then it_play game newStart opponent Computer
-           else if line == "2"
-               then return turn
-           else playDots game newStart opponent turn
--}
-
+-- return the next world given the game, current result, opponent, current turn and a move the player can make 
 me_play :: Game -> Result -> Player -> Turn -> Bar -> (Result, Turn)
 -- opponent has played, the person must now play
 me_play game (EndOfGame (-1)  start_state) opponent turn move =
@@ -54,7 +31,8 @@ me_play game (EndOfGame 1 start_state) opponent turn move =
 me_play game (ContinueGame state) opponent turn move =
         ((game move state), Computer)
 
-
+-- return the next world given the game, current result, opponent, current turn 
+--        the computer makes the move here. 
 it_play :: Game -> Result -> Player -> Turn -> (Result, Turn)
 -- person has played, the computer must now play
 it_play game (EndOfGame (-1)  start_state) opponent turn =
@@ -74,4 +52,5 @@ it_play game (ContinueGame state) opponent score =
         ((game opponent_move state), Person)
         where opponent_move = opponent state
 
+-- a possible opponent of the game
 simple_opponent (State board (h:t) n) = h
